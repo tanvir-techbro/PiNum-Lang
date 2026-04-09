e3551f36ff06df24cdbfc67ddd8f0e219ddfbfb6
tanvir-techbro <anjumtanvirmdabir.81@gmail.com>
2026-04-09 18:57:24 +0600

    fix: fixed bound checking in check_program_mode

--- src/helper.c
+++ src/helper.c
@@ -10,7 +10,7 @@ bool ENGINE_MODE = false;
 
 // NOTE: The below function is from include/mode.h
 bool check_program_mode(token_list *list) {
-        if (list->size == 2 && list->tokens[0].type == TOKEN_ATSIGN) {
+        if (list->size == 3 && list->tokens[0].type == TOKEN_ATSIGN) {
                 if (list->tokens[1].type == TOKEN_ID && list->tokens[2].type == TOKEN_ID) {
                         if (strcmp("for", list->tokens[1].value) == 0 && strcmp("engine", list->tokens[2].value)) {
                                 ENGINE_MODE = true;
