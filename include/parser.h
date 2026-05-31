8435fd0341ae45c7ab5945294962c69a306ec379
tanvir-techbro <anjumtanvirmdabir.81@gmail.com>
2026-05-31 17:17:48 +0600

    feat (parser): initialized parser

--- include/parser.h
+++ include/parser.h
@@ -4,4 +4,17 @@
 #include "ast.h"
 #include "lexer.h"
 
+typedef struct {
+        token_list *tokens;
+        size_t current;
+} Parser;
+
+// Core parser functions
+ASTnode *parse(token_list *tokens);
+
+// Internal parsing functions (recursive descent)
+ASTnode *parse_program(Parser *parser);
+ASTnode *parse_statement(Parser *parser);
+ASTnode *parse_expression(Parser *parser);
+
 #endif // !PARSER_H
