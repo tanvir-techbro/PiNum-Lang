bea9e491b3910d8356971af309fb892552a73116
tanvir-techbro <anjumtanvirmdabir.81@gmail.com>
2026-06-09 09:24:44 +0600

    chore (update): 0.5.0

--- include/version.h
+++ include/version.h
@@ -1,7 +1,7 @@
 #ifndef PINUM_VERSION_H
 #define PINUM_VERSION_H
 
-#define PINUM_VERSION "0.4.0"
+#define PINUM_VERSION "0.5.0"
 // update 0.3.5 added semicolon after every expression like javascript and C.
 
 #endif // PINUM_VERSION_H
