6010f2e6eb6a4b4ef23cdb43041d9df5363ccaea
tanvir-techbro <anjumtanvirmdabir.81@gmail.com>
2026-06-10 00:20:37 +0600

    chore (style): add copyright ascii banner to all the files

--- include/parser.h
+++ include/parser.h
@@ -1,3 +1,26 @@
+/********************************************************************
+ *   _____ _ _   _                       _                          *
+ *  |  __ (_) \ | |                     | |                         *
+ *  | |__) ||  \| |_   _ _ __ ___ ______| |     __ _ _ __   __ _    *
+ *  |  ___/ | . ` | | | | '_ ` _ \______| |    / _` | '_ \ / _` |   *
+ *  | |   | | |\  | |_| | | | | | |     | |___| (_| | | | | (_| |   *
+ *  |_|   | |_| \_|\__,_|_| |_| |_|     |______\__,_|_| |_|\__, |   *
+ *                                                          __/ |   *
+ *                                                         |___/    *
+ *                                                                  *
+ *  Copyright (c) 2026 tanvir-techbro.                              *
+ *  You may opt to use, copy, modify, merge, publish, distribute    *
+ *  and/or sell copies of the Software, and permit persons to whom  *
+ *  the Software is furnished to do so, under the conditions of the *
+ *  LICENSE.                                                        *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, *
+ *  EXPRESS OR IMPLIED.                                             *
+ *                                                                  *
+ *  If you find any bug you would be highly encouraged to create a  *
+ *  github issue at <https://github.com/tanvir-techbro/PiNum-Lang>  *
+ *  or contact <surjointelligence.team@gmail.com>                   *
+ ********************************************************************/
+
 #ifndef PARSER_H
 #define PARSER_H
 
